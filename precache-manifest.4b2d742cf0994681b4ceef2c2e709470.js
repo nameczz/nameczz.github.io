5508850b0e5d4e90b8b1409fe0a92dbab1c58897
nameczz <nameczz@126.com>
2019-09-09 16:25:30 +0800

    music version

---
+++ precache-manifest.4b2d742cf0994681b4ceef2c2e709470.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623f8bb535ef1575b9b64c4bad789a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7de99857d1f7c367e87",
+    "url": "/static/css/2.9582415c.chunk.css"
+  },
+  {
+    "revision": "640b22af117cce9e477a",
+    "url": "/static/css/main.9f78a98e.chunk.css"
+  },
+  {
+    "revision": "d7de99857d1f7c367e87",
+    "url": "/static/js/2.08045d55.chunk.js"
+  },
+  {
+    "revision": "640b22af117cce9e477a",
+    "url": "/static/js/main.b08a54ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a8a87a7d9d97d6ac2c8dd7388714f9b",
+    "url": "/static/media/music-icon.2a8a87a7.woff"
+  },
+  {
+    "revision": "453725f541422e39397accbade389cea",
+    "url": "/static/media/music-icon.453725f5.ttf"
+  },
+  {
+    "revision": "72f61f5619a4137f388581bc8ec1c34d",
+    "url": "/static/media/music-icon.72f61f56.eot"
+  },
+  {
+    "revision": "8a3485762b0fce80ad7e3d76262e65e7",
+    "url": "/static/media/music-icon.8a348576.svg"
+  }
+]);
